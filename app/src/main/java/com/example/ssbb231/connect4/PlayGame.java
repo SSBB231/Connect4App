d7592522d28820bebbdf74c67eb2c5738f1042c5
Eyou-Ab Mesfin <emesfin3@masonlive.gmu.edu>
2017-12-13 18:14:15 -0500

    added gridview to playgame activity & additional file to support it

--- app/src/main/java/com/example/ssbb231/connect4/PlayGame.java
+++ app/src/main/java/com/example/ssbb231/connect4/PlayGame.java
@@ -2,11 +2,24 @@ package com.example.ssbb231.connect4;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.widget.GridView;
+import android.widget.AdapterView;
+import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
 public class PlayGame extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                //connect with backend connect 4 game
+            }
+        });
     }
 }
